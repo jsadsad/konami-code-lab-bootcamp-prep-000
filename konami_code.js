b9167430c1d6d405bf5defc9837fd934eb544751
JCS <joshsad2@gmail.com>
2017-09-22 18:13:42 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,19 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
+let index = 0;
 
+function init(e) {
+  const key = parseInt(e.key);
+
+  if (key === code[index]) {
+    index++;
+
+    if(key === code.length) {
+      alert('You\'ve triggered the code!');
+
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
